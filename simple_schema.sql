1ae2d602e00e7c841629121db53fdd5f3a282da0
Alex Silverstein <aslvrstn@gmail.com>
2020-04-19 20:09:05 -0400

    Add a revision and query for it

--- simple_schema.sql
+++ simple_schema.sql
@@ -32,3 +32,9 @@ INSERT INTO data (state, data_date, tests, positive, checker, double_checker, pu
 INSERT INTO batch (push_time, shift_lead, commit_note, daily_commit) VALUES ("2020-03-21", "asilverstein", "Next day!", FALSE);
 INSERT INTO data (state, data_date, tests, positive, checker, double_checker, public_notes, batch_id) VALUES ("PA", "2020-03-21", 140, 78, "asilverstein", "jkodysh", "nothing to see here", 2);
 INSERT INTO data (state, data_date, tests, positive, checker, double_checker, public_notes, batch_id) VALUES ("NY", "2020-03-21", 212, 138, "asilverstein", "jkodysh", "still nothing to see here", 2);
+
+INSERT INTO batch (push_time, shift_lead, commit_note, daily_commit) VALUES ("2020-03-30", "asilverstein", "Revising old data", FALSE);
+INSERT INTO data (state, data_date, tests, positive, checker, double_checker, public_notes, batch_id) VALUES ("NY", "2020-03-20", 169, 99, "asilverstein", "jkodysh", "Missed one death", 3);
+
+/* Get the latest data for 2020-03-20 by state. Maybe this can be simpler -- I'm not very good at SQL. */
+SELECT * FROM (SELECT state, MAX(batch_id) as max_bid FROM data WHERE data_date='2020-03-20' GROUP BY state) AS x JOIN data WHERE data_date='2020-03-20' AND batch_id=x.max_bid AND data.state=x.state;
